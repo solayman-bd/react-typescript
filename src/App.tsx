9569d6ed682ef4829e50fc4bcf3c1e9904d161ec
solayman-bd <mdsolaymannuhin@gmail.com>
2021-07-23 15:24:59 +0600

    required state are defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
 
 function App() {
-  return <div className="App">Hi I am from TypeScript in react</div>;
+  const startTrivia = async () => {};
+  const checkAnswer = () => {};
+  const nextQuestion = () => {};
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const TOTAL_QUESTIONS = 10;
+
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions......</p>
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 }
 
 export default App;
