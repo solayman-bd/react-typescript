7ae145590907f20dd761a625ebaee303d1affb80
solayman-bd <mdsolaymannuhin@gmail.com>
2021-07-23 15:51:14 +0600

    utils.ts file added

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,4 @@
+import { shuffleArray } from "./utils";
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
@@ -11,11 +12,19 @@ export type Question = {
   question: string;
   type: string;
 };
+export type QuestionsState = Question & { answers: string[] };
 export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answer: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
   console.log(data);
 };
